3f31334bbcbe376b4940b53f852cad53a2412180
shamal melantha <shamalmelantha3@gmail.com>
2025-12-01 14:42:33 +0530

    update

--- api/location/[deviceId].js
+++ api/location/[deviceId].js
@@ -1,5 +1,5 @@
 import connectDB from "../../lib/db.js";
-import Location from "../../models/Location.js";
+import Location from "../../Models/location.js";
 
 export default async function handler(req, res) {
   await connectDB();
